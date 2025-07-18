744249d9b2dc4fc0cb6c4981cab4e8a98e09f96d
Anton Wolff <anton.wolff1@uni-bielefeld.de>
2025-07-18 16:29:30 +0200

    arschig

--- my_env.py
+++ my_env.py
@@ -1,5 +1,7 @@
 import numpy as np
+import scipy.io
 from epyt_flow.simulation import SensorConfig
+from pandas.core.common import require_length_match
 
 from env import WaterChlorinationEnv
 
@@ -19,6 +21,38 @@ def exponential_bound_violation_reward(nodes_concentration, upper_bound, lower_b
 
     return reward
 
+
+def infection_risk_reward(scada_data, contamination_data, threshold=0.1):
+    """
+    Computes a reward based on the infection risk in relation to a specified threshold.
+    If the infection risk is below the threshold, the reward is positive; otherwise, it is negative.
+    """
+
+    pathogen_concentration = scada_data.get_data_bulk_species_node_concentration(
+        {"P": contamination_data['all_junctions']})
+
+    if pathogen_concentration.size == 0 or np.all(pathogen_concentration == 0):
+        return 0.0
+
+    avg_consumption = 0.25
+
+    people_per_node = contamination_data['people_per_node']
+    r_entero = contamination_data['r_entero']
+
+    current_pathogen = pathogen_concentration[-1, :] if pathogen_concentration.ndim > 1 else pathogen_concentration
+    # TODO: Check if correct
+    dose_per_person = current_pathogen * avg_consumption
+    risk_per_person = 1.0 - np.exp(-r_entero * dose_per_person)
+
+    total_risk = np.sum(risk_per_person * people_per_node)
+    total_population = np.sum(people_per_node)
+    if total_population > 0:
+        infection_risk_percentage = total_risk / total_population * 100.0
+        print("infection risk is: ", infection_risk_percentage)
+        return -infection_risk_percentage
+
+    return 0.0
+
 class CustomWaterChlorinationEnv(WaterChlorinationEnv):
     """
     Custom control environment for water chlorination.
@@ -30,6 +64,14 @@ class CustomWaterChlorinationEnv(WaterChlorinationEnv):
                          f_in_streams_data=f_in_streams_data,
                          action_space=action_space)
         self.__sensor_config_reward = None
+
+        msx_mat = scipy.io.loadmat(f_in_contamination_metadata)
+        streams_mat = scipy.io.loadmat(f_in_streams_data)
+        self.contamination_data = {
+            "people_per_node": np.round(streams_mat["People_per_node"]).flatten(),
+            'all_junctions': [str(n[0]) for n in msx_mat["dist_nodes"].flatten().tolist()],
+            'r_entero': 0.014472,
+        }
 
     def _compute_reward_function(self, scada_data):
         """
@@ -44,16 +86,21 @@ class CustomWaterChlorinationEnv(WaterChlorinationEnv):
 
         if self.__sensor_config_reward is None:
             self.__sensor_config_reward = SensorConfig.create_empty_sensor_config(scada_data.sensor_config)
-            self.__sensor_config_reward.bulk_species_node_sensors = {"CL2": scada_data.sensor_config.nodes}
+            self.__sensor_config_reward.bulk_species_node_sensors = {"CL2": scada_data.sensor_config.nodes,
+                                                                     "P": self.contamination_data['all_junctions']}
         scada_data.change_sensor_config(self.__sensor_config_reward)
 
 
         reward = 0.0
 
-        reward = reward + exponential_bound_violation_reward(
-            nodes_concentration= scada_data.get_data_bulk_species_node_concentration({"CL2": scada_data.sensor_config.nodes}),
-            upper_bound= .4,  # Example upper bound for chlorine concentration
-            lower_bound= .2   # Example lower bound for chlorine concentration
+        # reward += exponential_bound_violation_reward(
+        #     nodes_concentration= scada_data.get_data_bulk_species_node_concentration({"CL2": scada_data.sensor_config.nodes})[0],
+        #     upper_bound= .4,  # Example upper bound for chlorine concentration
+        #     lower_bound= .2   # Example lower bound for chlorine concentration
+        # )
+
+        reward += infection_risk_reward(
+            scada_data, contamination_data=self.contamination_data
         )
 
         return reward
--- train.py
+++ train.py
@@ -1,14 +1,49 @@
+import os
+
+from datetime import datetime
 from gymnasium.wrappers import NormalizeObservation
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 from my_env import CustomWaterChlorinationEnv
 from scenarios import load_scenario
 
-if __name__ == '__main__':
+def make_env(scenario_id):
+    def _init():
+        env = CustomWaterChlorinationEnv(**load_scenario(scenario_id=scenario_id))
+        return NormalizeObservation(env)
+    return _init
 
-    with CustomWaterChlorinationEnv(**load_scenario(scenario_id=1)) as env:
+if __name__ == '__main__':
+    # os.makedirs("tensorboard_logs", exist_ok=True)
+    # os.makedirs("models", exist_ok=True)
+    #
+    # env_fns = [make_env(i) for i in range(10)]
+    # vec_env = DummyVecEnv(env_fns)
+    #
+    # timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # run_name = f"PPO_multi_scenario_0_9_{timestamp}"
+    #
+    # model = PPO(
+    #     "MlpPolicy",
+    #     vec_env,
+    #     verbose=1,
+    #     device="cpu",
+    #     tensorboard_log="tensorboard_logs"
+    # )
+    #
+    # model.learn(
+    #     total_timesteps=500_000,
+    #     tb_log_name=run_name
+    # )
+
+    # model.save(os.path.join("models", f"{run_name}.zip"))
+    # vec_env.close()
+
+    with CustomWaterChlorinationEnv(**load_scenario(scenario_id=4)) as env:
         model = PPO("MlpPolicy", NormalizeObservation(env), device="cpu")
-        model.learn(total_timesteps=100)
+        model.learn(total_timesteps=1000)
         model.save("first_ppo_model.zip")
+
 
